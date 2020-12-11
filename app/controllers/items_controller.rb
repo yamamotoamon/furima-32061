6b87b7af3223d4e01db8420e68bb849963782ab5
yamamotoamon <73935940+yamamotoamon@users.noreply.github.com>
2020-12-11 14:41:32 +0900

    削除機能の修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.user.id == current_user.id
+      @item.destroy
+    end
     redirect_to root_path
   end
 
