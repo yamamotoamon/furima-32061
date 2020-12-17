f193f7b14c341aaac0bf07e82af5b8953c3b3585
yamamotoamon <73935940+yamamotoamon@users.noreply.github.com>
2020-12-17 16:31:51 +0900

    削除機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  has_one :buy, foreign_key: :item_id, dependent: :destroy
+  has_one :buy, dependent: :destroy
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
